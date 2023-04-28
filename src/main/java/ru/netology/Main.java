2f59ac0d4c6411c79d028a77bf251fa7e2a2f06e
vOrzee <104573735+vOrzee@users.noreply.github.com>
2023-04-29 01:38:48 +0500

    Initial condition

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,14 @@
 package ru.netology;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
     }
 }
