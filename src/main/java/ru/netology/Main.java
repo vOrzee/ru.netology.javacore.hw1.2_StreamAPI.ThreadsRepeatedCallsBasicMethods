990f9085a9464ba3f14e17cbc576f4b974b3e3de
vOrzee <104573735+vOrzee@users.noreply.github.com>
2023-04-29 01:39:07 +0500

    Complete

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -9,6 +9,18 @@ public class Main {
     public static void main(String[] args) {
 
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
 
+        for(Integer elementInt : intList) {
+            if (elementInt > 0 && elementInt % 2 == 0) {
+                result.add(elementInt);
+            }
+        }
+
+        result.sort(Comparator.naturalOrder());
+
+        for(Integer elementResult : result) {
+            System.out.println(elementResult);
+        }
     }
 }
